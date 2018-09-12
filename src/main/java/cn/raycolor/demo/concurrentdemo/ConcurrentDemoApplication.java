15d99aaa46504a10a8f6e504236c26f3b6c59db2
leiyun951102@gmail.com <leiyun951102@gmail.com>
2018-09-12 13:54:56 +0800

    init

---
+++ src/main/java/cn/raycolor/demo/concurrentdemo/ConcurrentDemoApplication.java
@@ -0,0 +1,15 @@
+package cn.raycolor.demo.concurrentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leiyu
+ */
+@SpringBootApplication
+public class ConcurrentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentDemoApplication.class, args);
+	}
+}
